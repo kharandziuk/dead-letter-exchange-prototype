e9dbd1e27f4e79613eaacc361d90f52f98457631
Max Kharandziuk <max.kharandziuk@gmail.com>
2019-09-04 12:13:16 +0200

    fixed timeout in pipeMessages.js

--- pipeMessages.js
+++ pipeMessages.js
@@ -1,10 +1,39 @@
 const DLQ_NAME = 'DeadLetterQueue'
 const EXCHANGE_NAME = 'MainExchange'
 
+const delay = (ms) => new Promise((resolve) => {
+  setTimeout(resolve, ms)
+})
+
+
+const delayWithRefresh = (ms) => {
+  let flag = false
+  const promise = () => delay(ms).then(() => {
+    if (flag) {
+      return
+    } else {
+      flag = true
+      return promise()
+    }
+  })
+  const refresh = () => {
+    flag = false
+  }
+  return {
+    timeout: promise(),
+    refresh
+  }
+}
+
+
+
 require('amqplib').connect('amqp://localhost').then(async(connection) => {
     const channel = await connection.createChannel()
+    const dwr = delayWithRefresh(1000)
     channel.consume(DLQ_NAME, function(msg) {
         channel.publish(EXCHANGE_NAME, '',  Buffer.from(msg.content));
         channel.ack(msg)
+        dwr.refresh()
     });
+    dwr.timeout.then(() => process.exit(0))
 })
